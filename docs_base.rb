2ee156d707d7338b0f01ef82516b8c0047cf9606
GitHub Action <action@github.com>
2020-02-05 18:41:22 +0000

    Deployed 070ec34 with MkDocs version: 1.0.4

---
+++ docs_base.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "English"
+require 'dry-types'
+require 'dry-struct'
+
+Warning[:experimental] = false
+
+module Types
+  include Dry.Types()
+end
+
+module FakeDB
+  Rollback = Class.new(RuntimeError)
+
+  def self.transaction
+    yield
+  rescue Rollback
+    # doing rollback ...
+    raise
+  end
+end
+
+class User
+  def initialize(name:, age:)
+    @name, @age = name, age
+  end
+  attr_reader :name, :age
+
+  def save
+    !underage?
+  end
+
+  def errors
+    underage? ? [{ age: "underage" }] : nil
+  end
+
+  def underage?
+    @age <= 18
+  end
+end
