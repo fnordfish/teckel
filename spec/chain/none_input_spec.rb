09bd9157f0e02f532dfa07abd1ec73cf4b299bc5
Robert Schulze <robert@dotless.de>
2020-02-14 13:00:22 +0100

    Fix calling chain with settings and no input

---
+++ spec/chain/none_input_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+RSpec.describe Teckel::Chain do
+  module TeckelChainNoneInputTest
+    class MyOperation
+      include Teckel::Operation
+      result!
+
+      settings Struct.new(:say)
+
+      input none
+      output String
+      error none
+
+      def call(_)
+        settings&.say || "Called"
+      end
+    end
+
+    class Chain
+      include Teckel::Chain
+
+      step :a, MyOperation
+    end
+  end
+
+  specify "call chain without input value" do
+    result = TeckelChainNoneInputTest::Chain.call
+    expect(result.success).to eq("Called")
+  end
+
+  specify "call chain runner without input value" do
+    result = TeckelChainNoneInputTest::Chain.with(a: "What").call
+    expect(result.success).to eq("What")
+  end
+end
