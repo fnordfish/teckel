6fe28f7d69b6f249e96a25ecbae05e83069cc558
Robert Schulze <robert@dotless.de>
2025-08-28 13:00:13 +0200

    Fix dec tests

--- spec/doctest_helper.rb
+++ spec/doctest_helper.rb
@@ -4,5 +4,8 @@ require_relative "support/dry_base"
 require_relative "support/fake_db"
 require_relative "support/fake_models"
 
+lib = File.expand_path("../lib", __dir__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
 require "teckel"
 require "teckel/chain"
